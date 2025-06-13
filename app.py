ee4f19feb025a5ce22c6442d0eed6c5a333dbc9e
joilinsm <joilinsm@gmail.com>
2025-06-13 13:40:32 +0530

    commit

--- app.py
+++ app.py
@@ -72,4 +72,5 @@ def delete_item(item_id):
         return jsonify({"success": False, "error": str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use provided PORT or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
